a517a7de07dc576538ab842d5554b0d6bb1f3194
César <dev.pcesarmata@gmail.com>
2020-08-19 23:18:32 -0500

    Init commit

---
+++ src/main/java/com/cesarmata/ws30restfulwebservicesimplementinghetoas/Ws30RestfulWebServicesImplementingHetoasApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws30restfulwebservicesimplementinghetoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws30RestfulWebServicesImplementingHetoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws30RestfulWebServicesImplementingHetoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarmata/ws30restfulwebservicesimplementinghetoas/Ws30RestfulWebServicesImplementingHetoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws30restfulwebservicesimplementinghetoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws30RestfulWebServicesImplementingHetoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
